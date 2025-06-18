b5cb5d8e9ab3ecf576ba6be4920373202a7a9186
cynthialin1228 <cynthialin1228@gmail.com>
2025-06-19 03:30:49 +0800

    update infer.sh

--- run_infer_KUO.sh
+++ run_infer_KUO.sh
@@ -1,9 +1,10 @@
 design_name="design26"
+timestamp="2025-06-19_03-14-51"
 python3 infer_KUO.py \
-    --model ./trained_model/2025-06-18_16-11-57/saved_model_2025-06-18_16-11-57.pkl \
+    --model ./trained_model/${timestamp}/saved_model_${timestamp}.pkl \
     --features GraphSAINT/data/parsered_features/${design_name}/feat_full.npz \
     --adjacency GraphSAINT/data/parsered_features/${design_name}/adj_full.npz \
-    --config trained_model/2025-06-18_16-11-57/DATE21.yml \
+    --config trained_model/${timestamp}/DATE21.yml \
     --output ./output_infer_${design_name} \
     --gate_mapping GraphSAINT/data/parsered_features/${design_name}/gate_map.json \
     --iccad_output ./output_iccad_${design_name}.txt
